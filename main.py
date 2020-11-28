edd56d3a36c0d17a66fd9f55ba879cff8f452d85
sohamjaiswal10 <sohamjaiswal1@gmail.com>
2020-11-28 20:36:19 +0530

    chromecast can now be connected with voice

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import pychromecast
 import dictionaryAPI as dictAPI
 from winApps import winapps
 import chromecastchecker as ccc
+from lowerUtils import lowerUtils as lU 
 
 # One Class to rule em all
 # One Class to find them 
@@ -257,13 +258,13 @@ class voiceCommands:
         def __init__(self, device, fullcmd):
             print('Reached atleast here....')
             self.connection = False
-            self.device = device
+            self.device = self.cccComparator(device)
             if self.connection == False:
                 self.connect()
 
         def connect(self):
             try:
-                self.chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Den TV"])
+                self.chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[self.device])
                 print('reached here')
                 if self.chromecasts:
                     utilities.SpeakText('Connection succeeded!')
@@ -280,8 +281,19 @@ class voiceCommands:
                     self.connection = False
                     return
 
-    class mediaControls:
-        pass
+        def cccComparator(self, device):
+            with open('cache/chromecast.txt', 'r+') as f:
+                shit = f.readlines()
+                newshit=[]
+                for element in shit:
+                    newelement = element[0:-1]
+                    newshit.append(newelement)
+                f.close()
+            print(newshit)
+            self.device = (lU.fuzzyCompare(device, newshit))
+
+        class mediaControls:
+            pass
 
     class urban: 
 
