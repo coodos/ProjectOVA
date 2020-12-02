dcc0edf2000cd83fbe5b6dd64f3b80a6a94271b4
Coodos <codingrecoded@gmail.com>
2020-12-02 11:30:34 +0530

    incomplete filesearch

--- fileSearch.py
+++ fileSearch.py
@@ -18,13 +18,34 @@ class guiMethods():
         print(loc)
         fileName = fileNameEntry.get()
 
+        dirs = []
+
         for r, d, f in os.walk(loc):
             for file in f:
                 filePath = os.path.join(r, file)                
                 if file == fileName: 
                     print(filePath, 'found')
-                    os.startfile('/'.join(filePath.split('\\')[:-1]))
+                    dirs.append('/'.join(filePath.replace('\\', '/').split('/')[:-1]))
                     root.destroy() 
+                    if dirs != []:
+                        guiMethods.makeDirsPopup(dirs)
+
+    @staticmethod
+    def nextDir(dirs, currLabel):
+        pass
+
+    @staticmethod
+    def makeDirsPopup(dirs):
+        window = Tk()
+        window.geometry('400x300')
+        window.title('Found File in Dirs')
+
+        dirLabel = Label(window, text=dirs[0])
+        dirLabel.pack(pady=10, padx=10)
+        nextButton = Button(window, text='next', command=guiMethods.nextDir(dirs, dirLabel['text']), width=25)
+        nextButton.pack()
+
+
 
     # create the file dialog thing
     @staticmethod
