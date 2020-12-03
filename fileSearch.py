fd89fc4815f42237eeb95782de0c4ec3737b6c3c
Coodos <codingrecoded@gmail.com>
2020-12-03 21:20:35 +0530

    added file search

--- fileSearch.py
+++ fileSearch.py
@@ -4,6 +4,7 @@ import os
 
 class guiMethods():
 
+    dirnum = 0
     # get the directory
     @staticmethod
     def getPath():
@@ -18,6 +19,7 @@ class guiMethods():
         print(loc)
         fileName = fileNameEntry.get()
 
+        global dirs
         dirs = []
 
         for r, d, f in os.walk(loc):
@@ -26,26 +28,55 @@ class guiMethods():
                 if file == fileName: 
                     print(filePath, 'found')
                     dirs.append('/'.join(filePath.replace('\\', '/').split('/')[:-1]))
-                    root.destroy() 
-                    if dirs != []:
-                        guiMethods.makeDirsPopup(dirs)
+        print(dirs)
+        if dirs != []:
+            root.destroy() 
+            guiMethods.makeDirsPopup(dirs)
 
-    @staticmethod
-    def nextDir(dirs, currLabel):
-        pass
+    @classmethod
+    def nextDir(cls):
+        global dirs, dirLabel
+        try:
+            dirLabel["text"] = dirs[cls.dirnum + 1]
+            cls.dirnum += 1
+        except IndexError:
+            dirLabel["text"] = dirs[0]
+            cls.dirnum = 0
+    
+    @classmethod
+    def prevDir(cls):
+        global dirs, dirLabel
+        if cls.dirnum >= 1:
+            dirLabel["text"] = dirs[cls.dirnum - 1]
+            cls.dirnum -= 1
+        else:
+            dirLabel["text"] = dirs[len(dirs) - 1]
+            cls.dirnum = len(dirs) - 1
 
-    @staticmethod
-    def makeDirsPopup(dirs):
+    @classmethod
+    def openFolder(cls):
+        global dirs
+        os.startfile(dirs[cls.dirnum])
+
+    @classmethod
+    def makeDirsPopup(cls, dirs):
         window = Tk()
         window.geometry('400x300')
         window.title('Found File in Dirs')
 
-        dirLabel = Label(window, text=dirs[0])
+        Label(window, text="File found at", font=("arial", 30, "bold")).pack(pady=10, padx=10)
+
+        global dirLabel
+        dirLabel = Label(window, text=dirs[cls.dirnum])
         dirLabel.pack(pady=10, padx=10)
-        nextButton = Button(window, text='next', command=guiMethods.nextDir(dirs, dirLabel['text']), width=25)
+        nextButton = Button(window, text='next', command=guiMethods.nextDir, width=25, bg='#6d6d6d', fg='#fff')
         nextButton.pack()
 
+        backButton = Button(window, text='back', command=guiMethods.prevDir, width=25, bg='#6d6d6d', fg='#fff')
+        backButton.pack(padx=10, pady=10)
 
+        openButton = Button(window, text='Open Folder', command=guiMethods.openFolder, width=25, bg='#3d3d3d', fg='#fff')
+        openButton.pack(padx = 10, pady=10)
 
     # create the file dialog thing
     @staticmethod
